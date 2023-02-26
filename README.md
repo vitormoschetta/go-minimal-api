a2b68eff62565b1ad35e52f93d7fcaf2323e2023
Vitor <vitormoschetta@gmail.com>
2023-02-26 18:06:19 -0400

    add database

---
+++ README.md
@@ -0,0 +1,33 @@
+# Go Minimal API
+
+This is a minimal API example using Go and MySQL.
+
+## Requirements
+
+- Go 1.20
+- Docker
+- Docker Compose
+
+## Installation
+
+- Clone this repository
+- Install dependencies: `go mod tidy`
+- Run the database: `docker-compose up -d`
+- Run the API: `go run main.go`
+
+## Usage
+
+Sample requests:
+
+```bash
+curl --location 'http://localhost:8080/products'
+```
+
+```bash
+curl --location 'http://localhost:8080/products' \
+--header 'Content-Type: application/json' \
+--data '{    
+    "name": "MANGEIGA",
+    "price": 14
+}'
+```
--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/go-sql-driver/mysql"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 )
+
+var db *sql.DB
+
+var cfg = mysql.Config{
+	User:                 "go",
+	Passwd:               "go",
+	Net:                  "tcp",
+	Addr:                 "localhost:3306",
+	DBName:               "go",
+	AllowNativePasswords: true,
+}
 
 type Product struct {
 	ID    string  `json:"id"`
@@ -21,13 +37,20 @@ func NewProduct(name string, price float64) Product {
 	}
 }
 
-var products = []Product{
-	NewProduct("Apple", 1.99),
-	NewProduct("Orange", 2.99),
-	NewProduct("Banana", 3.99),
+func connectDB() {
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected to database")
 }
 
-func main() {
+func createRoutes() {
 	router := gin.Default()
 
 	router.GET("/", func(c *gin.Context) {
@@ -35,18 +58,26 @@ func main() {
 	})
 
 	router.GET("/products", func(c *gin.Context) {
+		products, err := FindAll()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
 		c.JSON(http.StatusOK, products)
 	})
 
 	router.GET("/products/:id", func(c *gin.Context) {
 		id := c.Param("id")
-		for _, product := range products {
-			if product.ID == id {
-				c.JSON(http.StatusOK, product)
-				return
-			}
+		product, err := FindByID(id)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
 		}
-		c.JSON(http.StatusNotFound, gin.H{"message": "Product not found"})
+		if product.ID == "" {
+			c.JSON(http.StatusNotFound, gin.H{"message": "Product not found"})
+			return
+		}
+		c.JSON(http.StatusOK, product)
 	})
 
 	router.POST("/products", func(c *gin.Context) {
@@ -56,39 +87,138 @@ func main() {
 			return
 		}
 		product = NewProduct(product.Name, product.Price)
-		products = append(products, product)
+		product, err := Create(product)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
 		c.JSON(http.StatusCreated, product)
 	})
 
 	router.PUT("/products/:id", func(c *gin.Context) {
 		id := c.Param("id")
-		for index, product := range products {
-			if product.ID == id {
-				var newProduct Product
-				if err := c.ShouldBindJSON(&newProduct); err != nil {
-					c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-					return
-				}
-				newProduct.ID = product.ID
-				products[index] = newProduct
-				c.JSON(http.StatusOK, newProduct)
-				return
-			}
+		product, err := FindByID(id)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
 		}
-		c.JSON(http.StatusNotFound, gin.H{"message": "Product not found"})
+		if product.ID == "" {
+			c.JSON(http.StatusNotFound, gin.H{"message": "Product not found"})
+			return
+		}
+		var newProduct Product
+		if err := c.ShouldBindJSON(&newProduct); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		newProduct.ID = product.ID
+		newProduct, err = Update(newProduct)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, newProduct)
 	})
 
 	router.DELETE("/products/:id", func(c *gin.Context) {
 		id := c.Param("id")
-		for index, product := range products {
-			if product.ID == id {
-				products = append(products[:index], products[index+1:]...)
-				c.JSON(http.StatusOK, gin.H{"message": "Product deleted"})
-				return
-			}
+		product, err := FindByID(id)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
 		}
-		c.JSON(http.StatusNotFound, gin.H{"message": "Product not found"})
+		if product.ID == "" {
+			c.JSON(http.StatusNotFound, gin.H{"message": "Product not found"})
+			return
+		}
+		err = Delete(id)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"message": "Product deleted"})
 	})
 
 	router.Run("localhost:8080")
+}
+
+func main() {
+	connectDB()
+	createRoutes()
+}
+
+func FindAll() ([]Product, error) {
+	rows, err := db.Query("SELECT id, name, price FROM products")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var products []Product
+	for rows.Next() {
+		var product Product
+		err := rows.Scan(&product.ID, &product.Name, &product.Price)
+		if err != nil {
+			return nil, err
+		}
+		products = append(products, product)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return products, nil
+}
+
+func FindByID(id string) (Product, error) {
+	var product Product
+	err := db.QueryRow("SELECT id, name, price FROM products WHERE id = ?", id).Scan(&product.ID, &product.Name, &product.Price)
+	if err != nil {
+		return product, err
+	}
+	return product, nil
+}
+
+func Create(product Product) (Product, error) {
+	res, err := db.Exec("INSERT INTO products (id, name, price) VALUES (?, ?, ?)", product.ID, product.Name, product.Price)
+	if err != nil {
+		return product, err
+	}
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		return product, err
+	}
+	if rowsAffected != 1 {
+		return product, fmt.Errorf("expected 1 row affected, got %d", rowsAffected)
+	}
+	return product, nil
+}
+
+func Update(product Product) (Product, error) {
+	res, err := db.Exec("UPDATE products SET name = ?, price = ? WHERE id = ?", product.Name, product.Price, product.ID)
+	if err != nil {
+		return product, err
+	}
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		return product, err
+	}
+	if rowsAffected != 1 {
+		return product, fmt.Errorf("expected 1 row affected, got %d", rowsAffected)
+	}
+	return product, nil
+}
+
+func Delete(id string) error {
+	res, err := db.Exec("DELETE FROM products WHERE id = ?", id)
+	if err != nil {
+		return err
+	}
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rowsAffected != 1 {
+		return fmt.Errorf("expected 1 row affected, got %d", rowsAffected)
+	}
+	return nil
 }
